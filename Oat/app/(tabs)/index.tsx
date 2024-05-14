78100ff2e75e1681f79c29f3d67ef9a2f25df619
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-05-14 10:35:08 +0000

    main

--- Oat/app/(tabs)/index.tsx
+++ Oat/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "@/components/Themed";
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>The OATICAR, BOOK OF THE GROATS AND MORE</Text>
+      <Text style={styles.title}>Behold! The oat abides! For now brothers, enjoy our oatic texts before the coming of Oatman.</Text>
     </View>
   );
 }
